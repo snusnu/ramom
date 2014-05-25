39b2fb83e2d5f6a8c9529062dd0cefb20444dda7
snusnu <gamsnjaga@gmail.com>
2014-05-25 20:03:29 +0200

    Fix indentation

--- lib/ramom/schema/mapping/natural_join.rb
+++ lib/ramom/schema/mapping/natural_join.rb
@@ -3,20 +3,20 @@
 module Ramom
   class Schema
     class Mapping
-        class NaturalJoin
-          include Concord.new(:fk_attributes)
+      class NaturalJoin
+        include Concord.new(:fk_attributes)
 
-          def call(base_name, attribute_name)
-            return attribute_name if fk_attribute?(base_name, attribute_name)
-            :"#{Inflecto.singularize(base_name)}_#{attribute_name}"
-          end
+        def call(base_name, attribute_name)
+          return attribute_name if fk_attribute?(base_name, attribute_name)
+          :"#{Inflecto.singularize(base_name)}_#{attribute_name}"
+        end
 
-          private
+        private
 
-          def fk_attribute?(base_name, attribute_name)
-            fk_attributes.fetch(base_name, EMPTY_ARRAY).include?(attribute_name)
-          end
-        end # NaturalJoin
+        def fk_attribute?(base_name, attribute_name)
+          fk_attributes.fetch(base_name, EMPTY_ARRAY).include?(attribute_name)
+        end
+      end # NaturalJoin
     end # Mapping
   end # Schema
 end # Ramom

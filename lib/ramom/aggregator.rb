8a643992f93475cb7f7b65407700fe64d51c3c39
snusnu <gamsnjaga@gmail.com>
2014-07-29 01:25:18 +0200

    Don't rely on keys in Aggregator#count

--- lib/ramom/aggregator.rb
+++ lib/ramom/aggregator.rb
@@ -7,14 +7,19 @@ module Ramom
 
     def count(attribute = nil)
       relation.summarize { |r|
-        r.add(:count, r.send(attribute || key_attribute).count)
+        r.add(:count, r.send(attribute || required_attribute).count)
       }.one[:count]
     end
 
     private
 
-    def key_attribute
-      relation.header.keys.to_a.flatten.first.name
+    def required_attribute
+      # we cannot rely on the presence of keys, therefore
+      # we just take an arbitrary required attribute
+      #
+      # find out if it's really necessary to find a required
+      # attribute. Afaik, RA doesn't support NULL anyways
+      relation.header.to_a.detect { |a| a.required? }.name
     end
   end # Aggregator
 end # Ramom

c710b51b5bd6904777c778d5d6b9a1b601f82a70
snusnu <gamsnjaga@gmail.com>
2014-07-31 18:59:36 +0200

    Explain the use of object singleton methods

--- lib/ramom/operation/registry.rb
+++ lib/ramom/operation/registry.rb
@@ -14,8 +14,14 @@ module Ramom
 
       def register(name, options, &block)
         dresser = dressers[options.fetch(:dresser)]
+        op      = operation.new(name, environment, dresser)
 
-        op = operation.new(name, environment, dresser)
+        # We basically use the op instance as the context
+        # to evaluate the function given as &block. This
+        # is probably a bit unusual for typical OO, it is
+        # however perfectly valid ruby, and simply another
+        # way of doing (prototype oriented) functional OOP
+
         (class << op; self end).class_eval {
           define_method(:call, &block)
         }

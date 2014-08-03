c2710cd29389b6b5e48bd448254f1875e07d06b4
snusnu <gamsnjaga@gmail.com>
2014-08-03 19:47:11 +0200

    Nuke now uselsee DM.schema_definition_options

--- lib/ramom/dm.rb
+++ lib/ramom/dm.rb
@@ -7,14 +7,6 @@ module Ramom
       Schema::Definition::Builder.call(models, &block)
     end
 
-    def self.schema_definition_options(models)
-      dm_definition = Schema::Definition::Builder.call(models)
-      {
-        base:           dm_definition[:base_relations],
-        fk_constraints: dm_definition[:fk_constraints]
-      }
-    end
-
     def self.operation_environment(options)
       schema = Ramom::Schema.build(options.reject { |k| k.equal?(:models) })
       writer = Ramom::DM::Writer.build(options.fetch(:models))
